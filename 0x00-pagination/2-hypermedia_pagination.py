0729612fd39563759350cac652c1ce54d3a4359c
ActuaryEmma <emmanyakio@gmail.com>
2024-02-03 10:36:50 +0300

    Task 2

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+Task 1
+"""
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def index_range(self, page, page_size):
+        """
+        return a tuple of size two containing a start index and an end index
+        """
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return (start_index, end_index)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        page with default value 1 and page_size with default value 10.
+        """
+        assert page > 0
+        assert type(page) == int
+        assert type(page_size) == int
+        assert page_size > 0
+
+        start_index, end_index = self.index_range(page, page_size)
+        return self.dataset()[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+        returns a dictionary
+        """
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+                'page_size': len(data),
+                'page': page,
+                'data': data,
+                'next_page': next_page,
+                'prev_page': prev_page,
+                'total_pages': total_pages
+                }
