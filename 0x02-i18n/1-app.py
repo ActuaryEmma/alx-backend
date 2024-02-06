90f40854698239fe541b65e8d564ed45ec0e6aef
ActuaryEmma <emmanyakio@gmail.com>
2024-02-06 18:42:01 +0300

    documentation

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,11 +4,13 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 class Config():
     """Babel configuration"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,5 +21,3 @@ babel = Babel(app)
 def index():
     """render index.html"""
     return render_template("1-index.html")
-if __name__ == "__main__":
-    app.run(debug=True)
--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -3,11 +3,13 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 class Config():
     """Babel configuration"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,11 +21,8 @@ def index():
     """render index.html"""
     return render_template("2-index.html")
 
+
 @babel.localeselector
 def get_locale():
     """determine the best match with our supported languages"""
     return (request.accept_languages.best_match(app.config['LANGUAGES']))
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
