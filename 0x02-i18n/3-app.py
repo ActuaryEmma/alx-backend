6e194113f00164ef5a0531dfd0612e08a30ea6ff
ActuaryEmma <emmanyakio@gmail.com>
2024-02-06 19:18:48 +0300

    Task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""task 2"""
+from flask import Flask, render_template
+from flask_babel import Babel, _
+
+
+class Config():
+    """Babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """render index.html"""
+    return render_template("3-index.html")
+
+
+def get_locale():
+    """determine the best match with our supported languages"""
+    return (request.accept_languages.best_match(app.config['LANGUAGES']))
