09ba0f2db15ec574f532e637307b23675d63851b
ActuaryEmma <emmanyakio@gmail.com>
2024-02-06 19:20:04 +0300

    added decorator

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -22,6 +22,7 @@ def index():
     return render_template("3-index.html")
 
 
+@babel.localeselector
 def get_locale():
     """determine the best match with our supported languages"""
     return (request.accept_languages.best_match(app.config['LANGUAGES']))
